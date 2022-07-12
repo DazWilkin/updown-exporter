9d385741b0403ee83d8982c50f99922aec5efefb
Daz Wilkin <daz.wilkin@gmail.com>
2022-07-11 19:21:13 -0700

    Updated

--- updown/types.go
+++ updown/types.go
@@ -24,34 +24,3 @@ type SSL struct {
 	Valid     bool   `json:"valid"`
 	Error     string `json:"error"`
 }
-
-// {
-// "token": "ngg8",
-// "url": "https://updown.io",
-// "alias": "",
-// "last_status": 200,
-// "uptime": 100,
-// "down": false,
-// "down_since": null,
-// "error": null,
-// "period": 15,
-// "apdex_t": 0.5,
-// "string_match": "",
-// "enabled": true,
-// "published": true,
-// "disabled_locations": [],
-// "recipients": ["email:1246848337", "sms:231178295"],
-// "last_check_at": "2021-12-17T05:00:01Z",
-// "next_check_at": "2021-12-17T05:00:16Z",
-// "mute_until": null,
-// "favicon_url": "https://updown.io/favicon.png",
-// "custom_headers": {},
-// "http_verb": "GET/HEAD",
-// "http_body": "",
-// "ssl": {
-// 	"tested_at": "2021-12-17T04:58:04Z",
-// 	"expires_at": "2022-02-21T15:57:36Z",
-// 	"valid": true,
-// 	"error": null
-// }
-// }
