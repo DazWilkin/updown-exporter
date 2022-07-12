861590b86828b0496ebc12684a8b53afca3cf938
DazWilkin <daz.wilkin@gmail.com>
2022-07-12 09:11:50 -0700

    Added `Metrics` type

---
+++ updown/metrics.go
@@ -0,0 +1,37 @@
+package updown
+
+// Metrics is a type that an updown Check's metrics
+type Metrics struct {
+	Apdex    float32  `json:"apdex"`
+	Requests Requests `json:"requests"`
+	Timings  Timings  `json:"timings"`
+}
+
+// Requests is a type that represents the Requests subtype of updown Metrics
+type Requests struct {
+	Samples        uint           `json:"samples"`
+	Failures       uint           `json:"failures"`
+	Satisfied      uint           `json:"satisfied"`
+	Tolerated      uint           `json:"tolerated"`
+	ByResponseTime ByResponseTime `json:"by_response_time"`
+}
+
+// ByResponseTime is a type that represents the ByResponseTime subtype of Requests
+type ByResponseTime struct {
+	Under125  uint `json:"under125"`
+	Under250  uint `json:"under250"`
+	Under500  uint `json:"under500"`
+	Under1000 uint `json:"under1000"`
+	Under2000 uint `json:"under2000"`
+	Under4000 uint `json:"under4000"`
+}
+
+// Timings is a type that represents the Timings subtype of updown Metrics
+type Timings struct {
+	Redirect   uint `json:"redirect"`
+	NameLookup uint `json:"namelookup"`
+	Connection uint `json:"connection"`
+	Handshake  uint `json:"handshake"`
+	Response   uint `json:"response"`
+	Total      uint `json:"total"`
+}
